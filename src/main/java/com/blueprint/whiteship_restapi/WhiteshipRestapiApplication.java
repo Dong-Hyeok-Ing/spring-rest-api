ac33035e2ecc102b091134677c2f47a6aa2074df
Dong-Hyeok-Ing <kdhhdk83@gmail.com>
2021-11-20 00:21:47 +0900

    첫번째 커밋입니다.
    프로젝트 생성 실행시 발새하는 에러 application.yml에 정리 했습니다.

---
+++ src/main/java/com/blueprint/whiteship_restapi/WhiteshipRestapiApplication.java
@@ -0,0 +1,14 @@
+package com.blueprint.whiteship_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteshipRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteshipRestapiApplication.class, args);
+        System.out.println(" 테스트 입니다. = ");
+    }
+
+}
---
+++ src/test/java/com/blueprint/whiteship_restapi/WhiteshipRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueprint.whiteship_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteshipRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
