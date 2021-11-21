519cb0175a260775f591464bac04f03ec2ea1fd0
KimDongHyuk <kdhhdk83@gmail.com>
2021-11-22 00:00:11 +0900

    입력값 제안하기 step_1
    EventDto로 파메메터를 받으면서 테스트가 깨졌다
    이력값 제안하기 step_2에서 해결방법을 찾아보자.

--- src/main/java/com/blueprint/whiteship_restapi/ebents/EventController.java
+++ src/main/java/com/blueprint/whiteship_restapi/ebents/EventController.java
@@ -1,5 +1,6 @@
 package com.blueprint.whiteship_restapi.ebents;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +17,24 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
-    private final EventRepository eventRepository;
+    private final EventRepository eventRepository; // interface는 전부 빈으로 등록되나. ?
+    private final ModelMapper modelMapper;
 
     //@Autowired 이벤트 Repository 10:40
     // 생성자로 사용 할 때는 생성자가 하나만 있고 생성자로 받아올 파라메터가 이미 빈으로 등록이 되어 있다면
     // 오토와이어드 어노테이션을 주지 안하도 된다. ! Spring 4.3 부터
-    public EventController(EventRepository eventRepository) {
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
 //    @Autowired
 //    EventRepository eventRepository;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
-
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createUri).body(event);
     }
